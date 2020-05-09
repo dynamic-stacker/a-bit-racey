af0e60b46fda17647ecf1f1f6e14a1b357bb8c6e
dynamic-stacker <65001857+dynamic-stacker@users.noreply.github.com>
2020-05-09 15:00:21 +0800

    Update a bit racey.py

--- a bit racey.py
+++ a bit racey.py
@@ -112,6 +112,9 @@ def lives_left(count):
 def things(thingx, thingy, thingw, thingh, block_color):
     pygame.draw.rect(gameDisplay, block_color, [thingx, thingy, thingw, thingh])
 
+def things2(thingx, thingy, thingw, thingh, block_color):
+    pygame.draw.rect(gameDisplay, block_color, [thingx, thingy, thingw, thingh])
+
 def gems_falling(gemx, gemy, gemw, gem_color):
     pygame.draw.circle(gameDisplay, gem_color, [gemx, gemy], gemw)
 
@@ -211,14 +214,21 @@ def description():
         pygame.display.update()
         clock.tick(15)
         
-def crash():
+def crash(car_id, play_mode):
     pygame.mixer.music.stop()
     pygame.mixer.Sound.play(crash_sound)
-    
-    largeText = pygame.font.Font('freesansbold.ttf',115)
-    TextSurf, TextRect = text_objects("You Crashed", largeText)
-    TextRect.center = ((display_width/2),(display_height/2))
-    gameDisplay.blit(TextSurf, TextRect)
+
+    if play_mode == 'single_player':
+        largeText = pygame.font.Font('freesansbold.ttf',115)
+        TextSurf, TextRect = text_objects("Crashed", largeText)
+        TextRect.center = ((display_width/2),(display_height/2))
+        gameDisplay.blit(TextSurf, TextRect)
+
+    if play_mode == 'multiplayer':
+        largeText = pygame.font.Font('freesansbold.ttf',115)
+        TextSurf, TextRect = text_objects("Player " + str(car_id) + "wins!", largeText)
+        TextRect.center = ((display_width/2),(display_height/2))
+        gameDisplay.blit(TextSurf, TextRect)
 
 
     while True:
@@ -229,6 +239,7 @@ def crash():
 
         #gameDisplay.fill(white)
         button("Play Again",150,450,105,50,green,bright_green,game_loop)
+        button("multi",150,550,100,50,green,bright_green,multiplayer)
         button("QUIT",550,450,100,50,red,bright_red,quitgame)
         button("Music",350,400,100,50,yellow,bright_yellow,music)
         button("Shop",350,500,100,50,blue,bright_blue,shop)
@@ -236,7 +247,7 @@ def crash():
 
         pygame.display.update()
         clock.tick(15)
-
+        
 def survived():
     global lives
     
@@ -591,7 +602,7 @@ def game_loop():
         high_score(h_score)
 
         if x > display_width - car_width or x < 0:
-            crash()
+            crash(1, "single_player")
             score = 0
             speed = 5
             s_buff = 0
@@ -656,27 +667,27 @@ def multiplayer():
     
     pygame.mixer.music.play(-1)
     
-    x1 = (display_width * 0.45)
-    x2 = (display_width * 0.45)
-    y = (display_height * 0.8)
+    x1 = (display_width * 0.20)
+    x2 = (display_width * 0.65)
+    y1 = (display_height * 0.8)
+    y2 = (display_height * 0.8)
 
+    x1_change = 0
+    x2_change = 0
 
-    x_change = 0
-
-    thing_startx = random.randrange(0, display_width)
-    thing_starty = -600
     thing_speed = 7
     thing_width = 100
     thing_height = 100
+    
+    thing_startx = random.randrange(0, round(mid_line - thing_width)) # make number integer
+    thing2_startx = thing_startx + 400
+    thing_starty = -600
 
     print_max_speed = True
 
     gameExit = False
 
     while not gameExit:
-
-##        if score > h_score:
-##            h_score = score
         
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -692,30 +703,42 @@ def multiplayer():
                         
             if event.type == pygame.KEYUP:
                 if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
-                    x_change = 0 
+                    x1_change = 0
+                if event.key == pygame.K_a or event.key == pygame.K_d:
+                    x2_change = 0 
 
         keys_pressed = pygame.key.get_pressed()
         if keys_pressed[pygame.K_LEFT]:
-            x_change = -speed
+            x1_change = -speed
         if keys_pressed[pygame.K_RIGHT]:
-            x_change = speed
+            x1_change = speed
+        if keys_pressed[pygame.K_a]:
+            x2_change = -speed
+        if keys_pressed[pygame.K_d]:
+            x2_change = speed
         
-        x1 += x_change   
-        x2 += x_change   
+        x1 += x1_change   
+        x2 += x2_change   
       
         gameDisplay.fill(white)
         pygame.draw.rect(gameDisplay, black, (mid_line,0,5,600))
 
         things(thing_startx, thing_starty, thing_width, thing_height, block_color)
+        things2(thing2_startx, thing_starty, thing_width, thing_height, block_color)
         thing_starty += thing_speed
         
-        car(x1,y)
-        car2(x2,y)
+        car(x1,y1)
+        car2(x2,y2)
         total_score(score1)
         total_score2(score2)
-        high_score(h_score)
 
-        if x > display_width - car_width or x < 0:
+        if x1 > mid_line - car_width or x1 < 0:
+            crash()
+            score1 = 0
+            score2 = 0
+            speed = 5
+
+        if x2 > display_width - car_width or x2 < mid_line :
             crash()
             score1 = 0
             score2 = 0
@@ -723,7 +746,12 @@ def multiplayer():
 
         if thing_starty > display_height:
             thing_starty = 0 - thing_height
-            thing_startx = random.randrange(0, display_width)
+            print(mid_line)
+            print(thing_width)
+            print(mid_line - thing_width)
+
+            thing_startx = random.randrange(0, round(mid_line - thing_width))
+            thing2_startx = thing_startx + 400
             score1 += 1
             score2 += 1
             
@@ -741,19 +769,20 @@ def multiplayer():
                     print("MAXIMUM SPEED REACHED")
                     print_max_speed = False
         
-        if y < thing_starty + thing_height:
+        if y1 < thing_starty + thing_height:
             if x1 > thing_startx and x1 < thing_startx + thing_width or x1 + car_width > thing_startx and x1 + car_width < thing_startx + thing_width:
                 if lives == 1:
                     speed = 5
                     score1 = 0
-                    crash()
+                    crash(2, "multiplayer")       
                     
-        if y < thing_starty + thing_height:
-            if x2 > thing_startx and x2 < thing_startx + thing_width or x2 + car_width > thing_startx and x2 + car_width < thing_startx + thing_width:
+        if y2 < thing_starty + thing_height:
+            if x2 > thing2_startx and x2 < thing2_startx + thing_width or x2 + car_width > thing2_startx and x2 + car_width < thing2_startx + thing_width:
                 if lives == 1:
                     speed = 5
                     score2 = 0
-                    crash()
+                    crash(1, "multiplayer")
+
                       
         pygame.display.update()
         clock.tick(60)
