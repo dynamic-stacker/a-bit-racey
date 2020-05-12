db8547b9388b531e97baceccfd1aed6dfd6b224b
dynamic-stacker <65001857+dynamic-stacker@users.noreply.github.com>
2020-05-12 15:28:54 +0800

    database tryout and testings

--- database_for_aBitRacey.py
+++ database_for_aBitRacey.py
@@ -4,4 +4,32 @@ conn = sqlite3.connect('database.db')
 
 c = conn.cursor()
 
-c.execute("""CREATE TABLE stats (name text, coins integer, gems integer, highscore integer)""")
+##c.execute("""CREATE TABLE stats
+##             (name text, coins integer, gems integer, highscore integer)""")
+
+##first_3_attempts = [('James', '42', '2', '35'),
+##                    ('Edwards', '34', '6', '23'),
+##                    ('Jayden', '88', '4', '78')
+##                    ]
+##
+##c.executemany('INSERT INTO stats VALUES (?,?,?,?)', first_3_attempts)
+
+def deleteRecord():
+    try:
+        # Deleting single record now
+        sql_delete_person = """DELETE FROM stats WHERE name = 'Edwards'"""
+        c.execute(sql_delete_person)
+        conn.commit()
+        print("Player record deleted successfully ")
+        c.close()
+
+    except sqlite3.Error as error:
+        print("Failed to delete player record from sqlite table", error)
+    finally:
+        if (conn):
+            conn.close()
+            print("the sqlite connection is closed")
+
+deleteRecord()
+
+
